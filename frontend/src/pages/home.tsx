e1e936f814c48eb5ca17382b3dc047dc323b6b47
Kushagra <kseth0808@gmail.com>
2025-03-07 11:19:31 +0530

    Initial commit

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -15,14 +15,19 @@ interface User {
 const Home = () => {
 
     const [users, setusers] = useState<User[]>([])
+    const [loading, setloading] = useState(false)
 
     useEffect(() => {
+        setloading(true)
         axios.get(getAllUsers)
             .then((res) => {
                 setusers(res.data?.users)
+                setloading(false)
                 console.log(res.data)
             })
             .catch((err) => {
+                setloading(false)
+                setusers(err)
                 console.log(err)
             })
     }, [])
@@ -56,23 +61,31 @@ const Home = () => {
                     <Button className=" bg-gray-800 text-white" onClick={handleAddUser}>Add User</Button>
                     <div className=" w-full border border-gray-300 mt-4 mb-10"></div>
                     <div className=" grid grid-cols-12 gap-4">
-                        {users.map((data, index) => (
-                            <div className=" col-span-3" key={index}>
-                                <div className=" w-full border border-gray-300 rounded-lg">
-                                    <div className=" w-full h-full flex-shrink-0">
-                                        <img src={data?.userProfileImage || ""} alt="" className=" w-full h-full object-cover rounded-t-lg" />
-                                    </div>
-                                    <div className=" p-4 w-full">
-                                        <div className=" text-lg font-medium">{data.userName}</div>
-                                        <div className=" text-sm">{data.userDescription}</div>
-                                        <div className=" w-full flex items-center justify-end gap-2 mt-4">
-                                            <Button className="  bg-gray-700 text-white" onClick={() => handleEdit(data.userId)}>Edit</Button>
-                                            <Button className=" bg-gray-700 text-white" onClick={() => handeDelete(data.userId)}>Delete</Button>
+                        {loading ? (
+                            <div className=" col-span-12">Loading...</div>
+                        ) : (
+                            <>
+                                {users.length > 0 ? users?.map((data, index) => (
+                                    <div className=" col-span-3" key={index}>
+                                        <div className=" w-full border border-gray-300 rounded-lg">
+                                            <div className=" w-full h-full flex-shrink-0">
+                                                <img src={data?.userProfileImage || ""} alt="" className=" w-full h-full object-cover rounded-t-lg" />
+                                            </div>
+                                            <div className=" p-4 w-full">
+                                                <div className=" text-lg font-medium">{data.userName}</div>
+                                                <div className=" text-sm">{data.userDescription}</div>
+                                                <div className=" w-full flex items-center justify-end gap-2 mt-4">
+                                                    <Button className="  bg-gray-700 text-white" onClick={() => handleEdit(data.userId)}>Edit</Button>
+                                                    <Button className=" bg-gray-700 text-white" onClick={() => handeDelete(data.userId)}>Delete</Button>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                        ))}
+                                )) : (
+                                    <div className="">No user found!</div>
+                                )}
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
