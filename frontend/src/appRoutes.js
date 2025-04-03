6d3a81495aeef096e8eacf4aacae29aa144c2711
Kushagra <kseth0808@gmail.com>
2025-04-03 19:33:15 +0530

    Intial commit

--- frontend/src/appRoutes.js
+++ frontend/src/appRoutes.js
@@ -1,4 +1,4 @@
-const host = 'http://localhost:3001';
+const host = 'https://leadmanagment.onrender.com';
 
 export const postSignUp = `${host}/data/signUp`
 export const getUserData = `${host}/data/userData`
